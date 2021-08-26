f6941fc11fd074fda82d0b68c3c7f77907126867
eric2788 <tsukiko852@gmail.com>
2021-08-26 14:11:43 +0800

    補充資料

--- eldependenci-mvc/src/main/java/com/ericlam/mc/eldgui/controller/UIController.java
+++ eldependenci-mvc/src/main/java/com/ericlam/mc/eldgui/controller/UIController.java
@@ -6,7 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 標記爲 UIController (控制器)
+ * 標記爲 UIController (控制器)。
+ * <br></br>
+ * 你需要定義一個名爲 index 的方法，參數自取所需，必須返回 {@link com.ericlam.mc.eldgui.view.BukkitView} 作爲控制器初始界面。
+ * <br></br>
+ * 然後新增自定義方法用於處理界面互動，方法必須有 {@link com.ericlam.mc.eldgui.event } 内的標注，參數各取所需，可返回 {@link com.ericlam.mc.eldgui.view.BukkitView}
+ * 或者 void
+ * <br></br>
+ * 可取參數詳情請到教學文件詳閱。
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
